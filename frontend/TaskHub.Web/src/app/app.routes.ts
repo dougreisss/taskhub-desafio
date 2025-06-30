93411bb8fffbb17e6d2918df7f213112835739a0
Douglas Reis <douglas_rrs@hotmail.com>
2025-06-30 18:11:35 -0300

    :sparkles: feat: add tailwindcss and task manager screen

--- frontend/TaskHub.Web/src/app/app.routes.ts
+++ frontend/TaskHub.Web/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { TaskManager } from './layout/task-manager/task-manager';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'task', pathMatch: 'full' },
+    { path: 'task',  component: TaskManager }
+];
--- frontend/TaskHub.Web/src/app/app.ts
+++ frontend/TaskHub.Web/src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +7,7 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
+
 export class App {
-  protected title = 'TaskHub.Web';
+
 }
